715b4334c40a803d67d56bd3f52fc8336228009f
mvcorrea <mvcorrea@gmail.com>
2020-04-02 02:45:15 -0300

    page not found handle

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const port = process.env.PORT || 5001;
 
 // react production path
-const react_static = path.join(__dirname, "client/build"); 
+const react_static = path.join(__dirname, "client/build");
 app.use(express.static(react_static));
 
 //// ROUTES
 // index.js from within the folder api resolves all requests
-app.use('/api', require("./api"));
+app.use("/api", require("./api"));
 
 // React Content, it only reaches here if does not match any prior line
-app.get("*", (req, res) => res.sendFile(react_static + "/index.html"));
+const indexPage = react_static + "index.html";
+app.get("*", (req, res) => {
+  fs.existsSync(indexPage) ? res.sendFile(indexPage) : res.sendStatus(404);
+});
 
-app.listen(port, () => console.log("app working on http://localhost:" + port));
+app.listen(port, () =>
+  console.log("app listening on http://localhost:" + port)
+);
